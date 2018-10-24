271e75bc1039e700ad516615c338c5b3c10a040a
niuruijing <850650367@qq.com>
2018-10-24 16:43:14 +0800

    add produce test git bransh

---
+++ testGitBranch/src/main/java/com/cloud/hj/GitBranchApplication.java
@@ -0,0 +1,10 @@
+package com.cloud.hj;
+
+/**
+ * Created by niuruijing on 2018/10/24.
+ */
+public class GitBranchApplication {
+    public static void main(String[] args) {
+        System.out.println("123");
+    }
+}
